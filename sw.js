30af041a556bca3c69bb94c4d75668c6ff62795e
Rıdvan Kavakoğlu <ridvankavakoglu18@gmail.com>
2026-07-12 14:12:20 +0300

    Fullscreen yatay bilgi kartı

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Offline PWA Desteği
    ============================================= */
 
-var CACHE_NAME = 'safak-haritasi-v7';
+var CACHE_NAME = 'safak-haritasi-v8';
 
 // Temel dosyalar (install sırasında cache'lenir)
 var CORE_FILES = [
