061e65f91a48fe5378d75b0944d92ab65af34588
Rıdvan Kavakoğlu <ridvankavakoglu18@gmail.com>
2026-07-20 17:48:43 +0300

    Yeni vers

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Offline PWA Desteği
    ============================================= */
 
-var CACHE_NAME = 'safak-haritasi-v20';
+var CACHE_NAME = 'safak-haritasi-v21';
 
 // Temel dosyalar (install sırasında cache'lenir)
 var CORE_FILES = [
